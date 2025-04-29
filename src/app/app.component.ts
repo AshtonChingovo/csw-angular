f9cb58611e64d1141fcee55ae1c448abeb87ee79
Ashton <ashtonchingovo@gmail.com>
2025-04-29 15:52:48 +0200

    Add: Transaction History

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { AppService } from './apps.service';
+import { CardproComponent } from "./cardpro/cardpro.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterModule,
+    CardproComponent
+],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'csw-angular';
+export class AppComponent implements OnInit {
+  isOAuthTokenCheckModalOpen = true;
+
+  redirectURL = '';
+
+  constructor(private appService: AppService) {}
+
+  ngOnInit(): void {
+    this.appService.response.subscribe((response) => {
+
+      if (response.isSuccessful) {
+        // if response != Success then backend has sent OAuth redirect URL
+        if (response.data.redirectUrl === "Success") {
+          this.isOAuthTokenCheckModalOpen = false;
+        } else {
+          this.redirectURL = response.data.redirectUrl;
+        }
+      }
+    });
+
+    // check the Google OAuthToken
+    this.appService.checkOAuthToken();
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    const formData = new FormData();
+
+    formData.append('file', file);
+  }
+
+  closeModal() {
+    this.isOAuthTokenCheckModalOpen = false;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { importProvidersFrom } from '@angular/core';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(AppRoutingModule),
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
+  ],
+})
   .catch((err) => console.error(err));
